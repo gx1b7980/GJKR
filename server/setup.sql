7401002cbe02d698a06819e39e22930dbfd1a155
Michael Rivera <mar558@drexel.edu>
2024-11-16 22:14:54 -0500

    Updated the setup.sql file to include transaction categories as well as storing transaction data created from server

--- server/setup.sql
+++ server/setup.sql
@@ -1,8 +1,37 @@
 CREATE DATABASE projectdemo;
 \c projectdemo;
-CREATE TABLE test (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(50),
-	balance INT
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    balance INT DEFAULT 0
 );
-INSERT INTO test (name, balance) VALUES ('alex', 100);
+INSERT INTO users (name, balance) VALUES 
+('alex', 100);
+
+-- Transaction categories
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+INSERT INTO categories (name) VALUES
+('groceries'),
+('entertainment'),
+('dining'),
+('bills'),
+('transportation'),
+('personal');
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    date TIMESTAMP NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    FOREIGN KEY (category) REFERENCES categories (name)
+);
+
+-- Insert some sample transactions data
+INSERT INTO transactions (date, amount, category) VALUES
+('2024-11-01 10:00:00', 50.00, 'groceries'),
+('2024-11-02 12:30:00', 20.00, 'entertainment'),
+('2024-11-03 14:00:00', 30.00, 'dining');
