b7fd647b4bfd96737cad4172fdec087d06bd7659
schen51103 <schen51103@gmail.com>
2024-11-18 12:36:02 -0500

    created a button modal for transaction creation instead of new package

--- frontend/js/overview.js
+++ frontend/js/overview.js
@@ -33,3 +33,26 @@ function displayTransactions(transactions) {
     }
   });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const openModalButton = document.getElementById("open-modal");
+  const modal = document.getElementById("transaction-modal");
+  const closeModalButton = document.getElementById("close-modal");
+
+  // Open modal
+  openModalButton.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
+
+  // Close modal
+  closeModalButton.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  // Close modal when clicking outside of it
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
